f365bfbb0cc3d4ef305ffee7dce1e289197b20f2
Christopher Archer <archer.chris@gmail.com>
2023-09-20 11:48:27 +0200

    Added avoid splitting inputs

--- addthem.zsh
+++ addthem.zsh
@@ -10,9 +10,11 @@ log_file="log.txt"
 : > $log_file
 
 # Add table header to the log file
-echo "+------------+---------------+---------+----------------+" >> $log_file
-echo "|  Question  |    Numbers    |  Guess  | Elapsed Time(s) |" >> $log_file
-echo "+------------+---------------+---------+----------------+" >> $log_file
+{
+  echo "+------------+---------------+---------+-----------------+"
+  echo "|  Question  |    Numbers    |  Guess  | Elapsed Time(s) |"
+  echo "+------------+---------------+---------+-----------------+"
+} >> $log_file
 
 while (( count < $max )); do
   echo Question $((count + 1)) / $max
@@ -21,12 +23,12 @@ while (( count < $max )); do
   length=$((RANDOM % 4 + 3))
 
   # Generate a random sequence of numbers with that length
-  nums=($(jot -r $length 1 9))
+  read -r -A nums <<< "$(jot -r $length 1 9 | tr '\n' ' ')"
 
   # Sort the nums array in ascending order
-  nums=($(printf "%s\n" "${nums[@]}" | sort -n))
+  read -r -A nums <<< "$(printf "%s\n" "${nums[@]}" | sort -n | tr '\n' ' ')"
 
-  echo ${nums[@]}
+  echo "${nums[@]}"
 
   # Record the start time
   start_time=$(date +%s)
@@ -61,13 +63,13 @@ while (( count < $max )); do
 
   # Calculate the sum of the numbers in the array
   sum=0
-  for n in ${nums[@]}; do
+  for n in "${nums[@]}"; do
     sum=$((sum + n))
   done
 
   # Log the question and elapsed time to a file, formatted as a table row
-  printf "| %10d | %13s | %7s | %14d |\n" $((count + 1)) "${nums[*]}" "$guess" "$elapsed_time" >> $log_file
-  echo "+------------+---------------+---------+----------------+" >> $log_file
+  printf "| %10d | %13s | %7s | %15d |\n" $((count + 1)) "${nums[*]}" "$guess" "$elapsed_time" >> $log_file
+  echo "+------------+---------------+---------+-----------------+" >> $log_file
 
   if [[ $guess -eq $sum ]]; then
     echo -e "\033[32;40mCorrect\033[0m"  # Green text on black background
